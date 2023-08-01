2ba4b232ec24703c14e93892511e702355dab53a
gencmucitler <sigmoid34@gmail.com>
2023-08-01 18:28:51 +0300

    Add files via upload

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ModbusLib.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ModbusLib.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=modbuslib.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/modbuslib.x.tar
